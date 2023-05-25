e6fe37773bd2c15aaebcf298747bb509dceb6e42
Vinay.Prashar <vinay.prashar@geminisolutions.com>
2023-05-25 13:50:48 +0530

    documenting

--- src/main/java/com/gemini/Main.java
+++ src/main/java/com/gemini/Main.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+// Exclude DataSourceAutoConfiguration only to be used when there is no database setup for the application
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Main {
     public static void main(final String[] args) {
